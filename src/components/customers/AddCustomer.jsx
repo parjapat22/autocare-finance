8439472696f28d3eb8aa50231b23740efec08741
Basant Lal <bsntparjapat8@gmail.com>
2025-02-25 13:53:13 +1000

    compound component implemented

--- src/components/customers/AddCustomer.jsx
+++ src/components/customers/AddCustomer.jsx
@@ -1,26 +1,41 @@
-import { useState } from "react";
 import NewCustomerForm from "./NewCustomerForm";
 
 import Button from "../Button";
 import Modal from "../../styles/Modal";
 
+// after compound component - Modal
 function AddCustomer() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal(!isOpenModal)}>
-        Add new customer
-      </Button>
+    <Modal>
+      <Modal.Open openModal="customer-form">
+        <Button>Add new customer</Button>
+      </Modal.Open>
 
-      {/* {isOpenModal && <NewCustomerForm />} */}
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <NewCustomerForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+      <Modal.Window windowName="customer-form">
+        <NewCustomerForm />
+      </Modal.Window>
+    </Modal>
   );
 }
 
 export default AddCustomer;
+
+// before compound component
+// function AddCustomer() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal(!isOpenModal)}>
+//         Add new customer
+//       </Button>
+
+//       {/* {isOpenModal && <NewCustomerForm />} */}
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <NewCustomerForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
--- src/styles/Modal.jsx
+++ src/styles/Modal.jsx
@@ -1,66 +1,97 @@
+import { useState, createContext, useContext, cloneElement } from "react";
 import { createPortal } from "react-dom";
+
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
 
 const StyledModal = styled.div`
+  background-color: var(--color-neutral-50);
+  padding: 3.2rem 4rem;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: var(--color-neutral-50);
+  border: 2px solid var(--color-indigo-500);
   /* border-radius: var(--border-radius-lg); */
-  padding: 3.2rem 4rem;
-  transition: all 0.5s;
 `;
 
 const Overlay = styled.div`
+  background-color: var(--color-backdrop);
+  width: 100%;
+  height: 100vh;
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100vh;
-  background-color: var(--color-backdrop);
   backdrop-filter: blur(2px);
   z-index: 1;
-  transition: all 0.5s;
 `;
 
 const Button = styled.button`
   background: none;
-  border: none;
-  padding: 0.4rem;
-  /* border-radius: var(--border-radius-sm); */
-  transform: translateX(0.8rem);
-  transition: all 0.2s;
   position: absolute;
+  top: 1rem;
+  right: 1rem;
+  transition: all 0.2s;
   cursor: pointer;
-  top: 1.2rem;
-  right: 1.9rem;
 
   &:hover {
     color: var(--color-red-500);
   }
 
   & svg {
-    width: 2.4rem;
-    height: 2.4rem;
+    width: 2.5rem;
+    height: 2.5rem;
     color: var(--color-grey-500);
   }
 `;
 
-function Modal({ children, onClose }) {
+// 1. create a context
+const ModalContext = createContext();
+
+// 2. create parent component
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  const open = setOpenName;
+  const close = () => setOpenName("");
+
+  return (
+    <ModalContext.Provider value={{ openName, open, close }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+// 3. create child components to implement the common task using useContext
+function Open({ children, openModal }) {
+  const { open } = useContext(ModalContext);
+
+  // clone element clones the component and add properties to it
+  return cloneElement(children, { onClick: () => open(openModal) });
+}
+
+function Window({ children, windowName }) {
+  const { openName, close } = useContext(ModalContext);
+
+  if (windowName !== openName) return null;
+
+  // create portal puts component on top of dom tree while at the correct position in component tree
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onClose}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
 
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
+
+// 4. add child components as properties to parent component
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
